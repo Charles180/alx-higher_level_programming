5bb4caea7e684451e60b1a4f8c1f274c0d26bf9b
Charles <charlesowino180@gmail.com>
2023-11-03 00:28:37 +0300

    infinite add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-
-import sys
-
-if __name__ == '__main__':
-    args = sys.argv[1:]  # Excluding the first argument (filename)
-    argument_summation = sum(int(arg) for arg in args)
-    print(argument_summation)
+if __name__ == "__main__":
+    import sys
+    result = 0
+    for arg in sys.argv:
+        if arg != sys.argv[0]:
+            result += int(arg)
+    print(result)
