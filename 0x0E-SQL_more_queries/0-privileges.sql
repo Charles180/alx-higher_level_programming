634398d6478c8918cec9647e377477254bf5c830
Charles <charlesowino180@gmail.com>
2024-01-17 14:59:57 -0800

    list all priviledges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- display all priviledges
-SHOW GRANTS FOR 'user_od_1'@'localhost';
+-- Lists all privileges of the MySQL users
+-- Query to list all privileges (GRANT)
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
