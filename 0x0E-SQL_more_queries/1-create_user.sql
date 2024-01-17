fa969c5a20d7a8b9222cbd3167360a75880df51f
Charles <charlesowino180@gmail.com>
2024-01-17 15:06:03 -0800

    create user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,7 @@
--- Creates an user in MySQL server
--- Create the user 'user_0d_1' in MySQL server
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- A script that creates 'user_0d_1' user 
+-- Gives the user above all the privileges
+-- sets the password for 'user_0d_1' to 'user_0d_1_pwd'
+-- Does not fail if 'user_0d_1' exists
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
