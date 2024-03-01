a2892b931545afd93d2cc5adc610c7f2a2863ad7
Charles <charlesowino180@gmail.com>
2024-03-01 15:14:08 -0800

    response header value

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Python script that takes URL sends request and displays value of X-Request-Id"""
+"""Takes URL sends request and displays value of X-Request-Id"""
 import urllib.request
 from sys import argv
 
